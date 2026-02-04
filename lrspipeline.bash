0a09bec30e27416d7df241e53e99724c9d18247c
Matt Johnson <matthew.j@wustl.edu>
2026-02-04 09:34:35 -0600

    comments in pipeline script

--- lrspipeline.bash
+++ lrspipeline.bash
@@ -39,23 +39,31 @@ JOB_GROUP_QC="/${USER}/compute-${COMPUTE_USER}/qc"
 # This checks if the submission was a workfile or a direct submission of a sample and adds them to an array
 if [[ -f $1 ]]; then FULLSMIDS=($(cat $1)); else FULLSMIDS=($@); fi
 for FULLSMID in ${FULLSMIDS[@]}; do
+
+# Set environment variables for processing sample
 export FULLSMID=$FULLSMID
 export INDIR=/storage1/fs1/${STORAGE_USER}/Active/${USER}/c1in/${FULLSMID}
 export OUTDIR=/storage1/fs1/${STORAGE_USER}/Active/${USER}/c1out/${FULLSMID}
 [ ! -d $OUTDIR ] && mkdir $OUTDIR
 BAM="${OUTDIR}/${FULLSMID}.merged.aln.srt.bam"
+export REF_FASTA=${REF_DIR}/GCA_009914755.4_T2T-CHM13v2.0_genomic.chr.fna
+
+# Set other variables for job submission
 export LSF_DOCKER_VOLUMES="/storage1/fs1/${STORAGE_USER}:/storage1/fs1/${STORAGE_USER} \
 /scratch1/fs1/${SCRATCH_USER}:/scratch1/fs1/${SCRATCH_USER} \
 /storage1/fs1/${STORAGE_USER}/Active/${USER}/c1in/LRS/REF:/ref \
 $HOME:$HOME"
-
-export REF_FASTA=${REF_DIR}/GCA_009914755.4_T2T-CHM13v2.0_genomic.chr.fna
-
 JOBNAME="ngi-${USER}-${FULLSMID}"
 LOGNAME="/scratch1/fs1/${SCRATCH_USER}/${USER}/c1out/logs/LRS/${FULLSMID}"
 
+## 1. Align
+# These jobs produce an aligned and sorted bam file.
+# It first attempts to align with a GPU. After 10 minutes, it runs a small job that exits.
+# After the exited job, it triggers the CPU alignment to run as a job array.
+# The CPU alignment requires merging, so that's done in a separate job that only runs if the array succeeds.
 export ALIGN_JOBS=$(find ${INDIR}/ -name "*.fastq.gz" | wc -l )
 
+# 1.1 GPU aligner
 bsub -g ${JOB_GROUP_GPU} \
     -J ${JOBNAME}-align-gpu \
     -n 8 \
@@ -67,6 +75,7 @@ bsub -g ${JOB_GROUP_GPU} \
     -q general \
     -a 'docker(mjohnsonngi/minimap2_gpu:1.0)' bash /scripts/minimap2_gpu.bash
 
+# 1.2 CPU aligner array
 bsub -g ${JOB_GROUP_ALIGN} \
     -J ${JOBNAME}-align-cpu[1-${ALIGN_JOBS}] \
     -w "exit(\"${JOBNAME}-align-gpu\")" \
@@ -79,6 +88,7 @@ bsub -g ${JOB_GROUP_ALIGN} \
     -q general \
     -a 'docker(mjohnsonngi/minimap2:1.0)' bash /scripts/align_fastq.bash
 
+# 1.3 CPU aligner merging
 bsub -g ${JOB_GROUP_ALIGN} \
     -J ${JOBNAME}-alignmerge \
     -w "done(\"${JOBNAME}-align-cpu\")" \
@@ -91,6 +101,9 @@ bsub -g ${JOB_GROUP_ALIGN} \
     -q general \
     -a 'docker(mjohnsonngi/minimap2:1.0)' bash /scripts/merge_bams.bash 
 
+## 2. Call SNVs
+# These jobs create a vcf with DeepVariant called SNVs and a phased vcf from Margin.
+# 2.1 Call SNVs with DeepVariant
 bsub -g ${JOB_GROUP} \
     -J ${JOBNAME}-deepvariant \
     -w "done(\"${JOBNAME}-alignmerge\") || done(\"${JOBNAME}-align-gpu\")" \
@@ -103,6 +116,7 @@ bsub -g ${JOB_GROUP} \
     -q general \
     -a 'docker(mjohnsonngi/deepvariant:1.0)' bash /scripts/run_deepvariant_bam.bash $BAM
 
+# 2.2 Phase vcf from DeepVariant with Margin
 bsub -g ${JOB_GROUP} \
     -J ${JOBNAME}-margin \
     -w "done(\"${JOBNAME}-deepvariant\")" \
@@ -115,6 +129,9 @@ bsub -g ${JOB_GROUP} \
     -q general \
     -a 'docker(mjohnsonngi/margin:1.0)' bash /scripts/run_margin.bash $BAM ${BAM%.*}.vcf.gz
 
+## 3. Call Structural Variants
+# This job runs Sniffles2 to find structural variants from an aligned bam.
+# 3.1 Call structural variants with Sniffles2
 bsub -g ${JOB_GROUP} \
     -J ${JOBNAME}-sniffles \
     -w "done(\"${JOBNAME}-alignmerge\") || done(\"${JOBNAME}-align-gpu\")" \
@@ -127,6 +144,9 @@ bsub -g ${JOB_GROUP} \
     -q general \
     -a 'docker(mjohnsonngi/sniffles:1.0)' bash /scripts/run_sniffles_bam.bash $BAM
 
+## 4. QC
+# These jobs provide QC metrics for LRS bam files.
+# 4.1 Find coverage with mosdepth
 bsub -g ${JOB_GROUP} \
     -J ${JOBNAME}-mosdepth \
     -w "done(\"${JOBNAME}-alignmerge\") || done(\"${JOBNAME}-align-gpu\")" \
@@ -139,6 +159,7 @@ bsub -g ${JOB_GROUP} \
     -q general \
     -a 'docker(mjohnsonngi/mosdepth:1.0)' bash /scripts/run_mosdepth.bash $BAM
 
+# 4.2 Run NanoPlot to get other metrics
 bsub -g ${JOB_GROUP} \
     -J ${JOBNAME}-nanoplot \
     -w "done(\"${JOBNAME}-alignmerge\") || done(\"${JOBNAME}-align-gpu\")" \
