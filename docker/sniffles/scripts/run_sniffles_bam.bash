aa7863ba892462c5408a8139de55cc1e318a83a4
Matt Johnson <matthew.j@wustl.edu>
2026-02-04 10:24:29 -0600

    compress and index sniffles output

--- docker/sniffles/scripts/run_sniffles_bam.bash
+++ docker/sniffles/scripts/run_sniffles_bam.bash
@@ -6,4 +6,6 @@ sniffles \
 	-v ${BAM%.bam}.sniffles.vcf \
 	--snf ${BAM%.bam}.sniffles.snf \
 	-t 8 \
-	--minsvlen 25
+	--minsvlen 25 \
+&& bgzip ${BAM%.bam}.sniffles.vcf \
+&& tabix ${BAM%.bam}.sniffles.vcf.gz
